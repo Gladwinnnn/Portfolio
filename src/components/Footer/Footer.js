2d3110cc2f4e313103947d9cf1c27a25b6cb9561
Gladwinnnn <gladwin1503@hotmail.com>
2021-08-27 21:36:40 +0800

    Update Footer.js

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -17,7 +17,8 @@ const Footer = () => {
       </LinkList>
       <SocialIconsContainer>
         <CompanyContainer>
-          <Slogan>"The best investment you can make, is an investment in yourself" -Warren Buffett</Slogan>
+          <Slogan>"The best investment you can make, is an investment in yourself"</Slogan>
+          <Slogan>                                                                 -Warren Buffett</Slogan>
         </CompanyContainer>
         <SocialContainer>
           <SocialIcons href="https://github.com/Gladwinnnn">
